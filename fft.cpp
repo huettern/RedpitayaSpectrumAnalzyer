e48528f076ae670a77afb82065fb0f324192cd1f
noah95 <noahhuetter@gmail.com>
2015-04-09 16:35:44 +0200

    initial

--- fft.cpp
+++ fft.cpp
@@ -1,12 +1,31 @@
 #include "fft.h"
 
-FFT::FFT(QObject *parent) : QObject(parent)
+FFT::FFT(QObject *parent, QCustomPlot *ptrplot) : QObject(parent)
 {
-
+    plot = ptrplot;
 }
 
 FFT::~FFT()
 {
 
 }
+
+/**
+ * @brief singleConversion
+ *
+ * Calculates the FFT of a single set of Data
+ */
+void FFT::singleConversion()
+{
+    // Get Data
+
+
+    // Calculate Data
+
+    // Display Data
+
+}
+
+
+
 
--- fft.h
+++ fft.h
@@ -2,17 +2,30 @@
 #define FFT_H
 
 #include <QObject>
+#include "qcustomplot.h"
+
+class QCustomPlot;
 
 class FFT : public QObject
 {
     Q_OBJECT
 public:
-    explicit FFT(QObject *parent = 0);
+    explicit FFT(QObject *parent = 0, QCustomPlot *ptrplot = 0);
     ~FFT();
+
+    void singleConversion();
+
+private:
+    QCustomPlot *plot;
+
 
 signals:
 
+
+
 public slots:
+
+
 };
 
 #endif // FFT_H
