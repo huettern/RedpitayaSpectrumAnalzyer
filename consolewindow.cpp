87928e9845d1702b83a67ef4e3c5026408898302
noah95 <noahhuetter@gmail.com>
2015-04-09 09:31:11 +0200

    Added a console window

---
+++ consolewindow.cpp
@@ -0,0 +1,55 @@
+#include "consolewindow.h"
+#include "ui_consolewindow.h"
+
+#include <QScrollBar>
+#include <QDebug>
+
+ConsoleWindow::ConsoleWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::ConsoleWindow)
+{
+    ui->setupUi(this);
+
+    ui->consoleBox->document()->setMaximumBlockCount(100);
+    QPalette p = palette();
+    p.setColor(QPalette::Base, Qt::black);
+    p.setColor(QPalette::Text, Qt::gray);
+    setPalette(p);
+    ui->consoleBox->setPalette(p);
+    ui->commandLine->setPalette(p);
+}
+
+ConsoleWindow::~ConsoleWindow()
+{
+    delete ui;
+}
+
+void ConsoleWindow::putData(const QByteArray &data)
+{
+    qDebug() << "Put";
+
+//    for(int i = 0; i < data.size(); i++)
+//    {
+//        // if we already are in a esc seq, clear byte if not end
+//        if(inEcapeSeq)
+//        {
+//            if(data[i] == )
+//        }
+
+//        //check if escpae begin char
+//        if(data[i] == 0x1B)
+//    }
+
+    ui->consoleBox->insertPlainText(QString(data).toLatin1());
+
+    QScrollBar *bar = ui->consoleBox->verticalScrollBar();
+    bar->setValue(bar->maximum());
+}
+
+
+
+void ConsoleWindow::on_sendButton_clicked()
+{
+    QString str = ui->commandLine->text() + '\n';
+    emit getData(str.toLocal8Bit());
+}
---
+++ consolewindow.h
@@ -0,0 +1,36 @@
+#ifndef CONSOLEWINDOW_H
+#define CONSOLEWINDOW_H
+
+#include <QMainWindow>
+
+#include <QPlainTextEdit>
+
+namespace Ui {
+class ConsoleWindow;
+}
+
+class ConsoleWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit ConsoleWindow(QWidget *parent = 0);
+    ~ConsoleWindow();
+
+    void putData(const QByteArray &data);
+
+protected:
+
+private:
+    Ui::ConsoleWindow *ui;
+    // is set if the received data is currently in a escape seq
+    bool inEcapeSeq;
+
+signals:
+    void getData(const QByteArray &data);
+
+private slots:
+    void on_sendButton_clicked();
+};
+
+#endif // CONSOLEWINDOW_H
