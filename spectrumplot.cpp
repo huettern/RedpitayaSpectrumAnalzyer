76b17421b0618234d6c7402520963e548e0ad57a
noah95 <noahhuetter@gmail.com>
2015-05-08 15:49:17 +0200

    stuff

--- spectrumplot.cpp
+++ spectrumplot.cpp
@@ -22,7 +22,7 @@ void SpectrumPlot::setPlot(QCustomPlot *qcplot)
     // Set design parameter
     // create graph and assign data to it:
     m_plot->addGraph();
-    m_plot->graph(0)->setPen(QPen(QColor(0x99, 0xff, 0x33, 160), 2));
+    m_plot->graph(0)->setPen(QPen(QColor(0x99, 0xff, 0x33, 160), 1));
     //m_plot->graph(0)->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, QPen(Qt::black, 1.5), QBrush(Qt::white), 5));
 
     // give the axes some labels:
